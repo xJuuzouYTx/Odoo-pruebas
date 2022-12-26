8c72cbbfb4b34679dce9fdaad766595d1aebf973
Nimbutech1 <hugo.gonzalez@nimbutech.com>
2022-12-26 14:05:26 +0000

    [FIX] ERROR LOGIN

--- custom_modules/models/.ipynb_checkpoints/models-checkpoint.py
+++ custom_modules/models/.ipynb_checkpoints/models-checkpoint.py
@@ -1,17 +1,22 @@
 # Importar la librería de Odoo
 import odoo
 
+# Importar la librería de Odoo
+import odoo
+
 # Iniciar sesión en Odoo
-odoo.api.Environment.open().cr.commit()
+with odoo.api.Environment.manage():
+    # Buscar el diario de ventas que utilizas para las facturas rectificativas
+    journal = odoo.env['account.journal'].search([('type', '=', 'sale_refund')])
 
-# Buscar el diario de ventas que utilizas para las facturas rectificativas
-journal = odoo.env['account.journal'].search([('type', '=', 'sale_refund')])
+    # Buscar el diario de ventas que utilizas para las facturas rectificativas
+    journal = odoo.env['account.journal'].search([('type', '=', 'sale_refund')])
 
-# Cambiar la cuenta de ingreso utilizada en el diario de ventas
-journal.default_credit_account_id = ID_DE_LA_CUENTA_4175
+    # Cambiar la cuenta de ingreso utilizada en el diario de ventas
+    journal.default_credit_account_id = ID_DE_LA_CUENTA_4175
 
-# Guardar los cambios en el diario de ventas
-journal.save()
+    # Guardar los cambios en el diario de ventas
+    journal.save()
 
-# Cerrar la sesión de Odoo
-odoo.api.Environment.manage().logout()
+    # Cerrar la sesión de Odoo
+    odoo.api.Environment.manage().logout()
