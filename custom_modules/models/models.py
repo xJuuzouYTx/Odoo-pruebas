049070e491ee8f44398d3499c2a319acdbe777b3
Nimbutech1 <hugo.gonzalez@nimbutech.com>
2022-12-26 18:10:23 +0000

    [FIX] ERROR LOGIN

--- custom_modules/models/models.py
+++ custom_modules/models/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError
 
 
 class AccountMoveReversalInherit(models.TransientModel):
@@ -8,4 +9,4 @@ class AccountMoveReversalInherit(models.TransientModel):
     
     
     def create(self, vals):
-        raise "Error al crear"
+        raise UserError("Error al crear")
