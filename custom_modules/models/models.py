76affae61f53e0f7428041e107dbb54a4b869ac8
Nimbutech <alexander.aldana@nimbutech.com>
2022-12-26 13:44:37 -0500

    test

--- custom_modules/models/models.py
+++ custom_modules/models/models.py
@@ -11,8 +11,8 @@ class AccountMoveReversalInherit(models.TransientModel):
     def create(self, vals):
         print("********************************", self)
         # Ejecute la acción deseada antes de crear el nuevo registro
-        result = super(AccountMoveReversalInherit, self).create(vals)
+        #result = super(AccountMoveReversalInherit, self).create(vals)
         
-        #raise UserError("Error al crear")
+        raise UserError("Error al crear")
         # Ejecute la acción deseada después de crear el nuevo registro
         return result
