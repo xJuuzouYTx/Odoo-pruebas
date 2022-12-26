a6ce4f4fac9bb31135acf5acfc1a645274484a4d
Nimbutech1 <hugo.gonzalez@nimbutech.com>
2022-12-26 18:21:53 +0000

    [FIX] ERROR LOGIN

--- custom_modules/models/models.py
+++ custom_modules/models/models.py
@@ -2,11 +2,16 @@
 
 from odoo import models, fields, api
 from odoo.exceptions import UserError
-
-
-class AccountMoveReversalInherit(models.TransientModel):
+    
+class AccountMoveReversalInherit(models.Model):
     _inherit = 'account.move.reversal'
-    
-    
+
+    # Campos del modelo heredado
+
     def create(self, vals):
+        # Ejecute la acción deseada antes de crear el nuevo registro
+        result = super(AccountMoveReversalInherit, self).create(vals)
+        
         raise UserError("Error al crear")
+        # Ejecute la acción deseada después de crear el nuevo registro
+        return result
