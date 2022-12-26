282cbfc5f8bcb2a3afc68411a6d246307f876e98
Nimbutech1 <hugo.gonzalez@nimbutech.com>
2022-12-26 16:48:54 +0000

    [FIX] ERROR LOGIN

---
+++ custom_modules/models/.ipynb_checkpoints/models-checkpoint.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class AccountMoveReversalInherit(models.TransientModel):
+    _inherit = 'account.move.reversal'
+    
+    
+    def write(self, vals):
+        print(write)
