41e782b6067cfb8cf9ae68a09a5d3b2ce0d0283e
Nimbutech <alexander.aldana@nimbutech.com>
2022-12-26 16:07:47 -0500

    test

--- custom_modules/models/models.py
+++ custom_modules/models/models.py
@@ -46,7 +46,7 @@ class AccountMoveReversalInherit(models.TransientModel):
             moves_to_redirect |= new_moves
 
         self.new_move_ids = moves_to_redirect
-        raise UserError(self.new_move_ids)
+        raise UserError(self.new_move_ids.invoice_line_ids)
 
         # Create action.
         action = {
